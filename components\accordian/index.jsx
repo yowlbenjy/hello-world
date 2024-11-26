fce0e220b29ebb09dd923a3395881c53b8d9df58
yoel benjamin <yoelbenjamin4@gmail.com>
2024-11-25 19:15:53 -0500

    Youtube Subscribe button Rock paper Scissors Amazon shipping calculator

---
+++ components\accordian/index.jsx
@@ -0,0 +1,8 @@
+
+
+
+export default function Accordian(){
+  return <div>
+    Accordian
+  </div>
+}
---
+++ hello-website.js
@@ -0,0 +1,10 @@
+import ./i
+
+
+function App(){
+  return(
+    <div className="App">
+      <Accordian/>
+    </div>
+  )
+}
