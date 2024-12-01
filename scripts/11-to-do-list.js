818da72cbd789bf49c4cc73dd70de85b923719db
yoel benjamin <yoelbenjamin4@gmail.com>
2024-12-01 13:22:50 -0500

    Updated arrays & loops

--- scripts/11-to-do-list.js
+++ scripts/11-to-do-list.js
@@ -1,21 +1,32 @@
-const todoList = [];
+const todoList = [{
+  task: 'make dinner',
+  dueDate: '2022-12-22'
+}, {
+  task: 'wash',
+  dueDate: '2022-12-22'
+}];
+
+renderList();
 
 function addtoList() {
-  
+
   const inputElement = document.querySelector('.js-input-element');
-  const inputValue = inputElement.value;
-  todoList.push(inputValue);
+  const task = inputElement.value;
+
+  const dateinputElement = document.querySelector('.js-date-input-element');
+  const dueDate = dateinputElement.value;
+
+
+  todoList.push({
+    task,
+    dueDate
+  });
   inputElement.value = '';
+  dateinputElement.value = '';
 
   let todoListHTML = '';
-
-  for (let i = 0; i < todoList.length; i++) {
-    const todo = todoList[i];
-    const html = `<p>${todo}</p>`;
-    todoListHTML += html;
-  }
-
   document.querySelector('.js-list').innerHTML = todoListHTML;
+  renderList();
 }
 
 // Add an event listener for the Enter key
@@ -30,8 +41,17 @@ function renderList() {
   let todoListHTML = '';
 
   for (let i = 0; i < todoList.length; i++) {
-    const todo = todoList[i];
-    const html = `<p>${todo}</p>`;
+    const todoObject = todoList[i];
+    const { task, dueDate } = todoObject;
+    //const date = todoObject.dueDate;
+    const html = `
+      <div>${task}</div>
+      <div>${dueDate}</div>
+      <button class="delete-button" onclick = "
+        todoList.splice(${i},1);
+        renderList();
+        ">Delete</button>
+      `;
     todoListHTML += html;
   }
 
